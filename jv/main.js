d6fa1eee767bcc97de31fe8d8cec901cfccdede9
oplawlz <thealmightymonky@gmail.com>
2018-01-26 17:27:17 -0500

    Added zip field. Added zip validation. Added zip field invalid on blur.

--- jv/main.js
+++ jv/main.js
@@ -1,2 +1,36 @@
-// When the user scrolls the page, execute myFunction
-console.log("confirmed reading the js file");
+
+$(document).ready(function(){
+var zform = document.getElementById("zip");
+zform.addEventListener("blur", function (event){
+  var zipVerify = 'http://api.zippopotam.us/us/' + document.getElementById("zip").value ;
+  fetch(zipVerify)
+  .then(response => {
+    if(response.status != 200){
+      document.getElementById("zip").classList.remove("is-valid");
+    document.getElementById("zip").classList.add("is-invalid");
+  } else{
+    document.getElementById("zip").classList.remove("is-invalid");
+    document.getElementById("zip").classList.add("is-valid");
+  }
+  })
+  .then(data => {
+  });
+});
+});
+
+// $(document).ready(function(){
+// var zform = document.getElementById("zip");
+// zform.addEventListener("blur", function (event){
+//   var zipVerify = 'http://api.zippopotam.us/us/' + document.getElementById("zip").value ;
+//   fetch(zipVerify)
+//   .then(response => {
+//     if(response.status != 200){
+//     $("zip").addClass("is-invalid");
+//   } else{
+//     $("zip").addClass("is-valid");
+//   }
+//   })
+//   .then(data => {
+//   });
+// });
+// });
